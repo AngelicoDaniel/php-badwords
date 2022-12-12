6e78ec9bd0d01a154a24b437132669a2e3be1167
Daniel <angelicodaniel93@gmail.com>
2022-12-12 15:16:02 +0100

    paragrafo scritto in php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$paragraph = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit quo voluptates voluptatem possimus quae ex praesentium consequuntur, voluptatum modi! Quisquam, beatae consequuntur aliquam aut minima enim culpa quos sapiente aperiam?'
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Words</title>
+</head>
+<body>
+    <p><?php echo $paragraph ?></p>
+</body>
+</html>
